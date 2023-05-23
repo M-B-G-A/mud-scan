71d56cd4224ba4b12c2252d70d31f48786808454
Hakyung Kim <hkhk4862@gmail.com>
2023-05-23 20:54:55 +0900

    topuser > indent

--- packages/client/src/TopUser.tsx
+++ packages/client/src/TopUser.tsx
@@ -1,40 +1,41 @@
-import { Container, List, ListItem, ListItemText, alpha } from '@mui/material';
+import { List, ListItem } from '@mui/material';
 import { useMUD } from "./MUDContext";
 import { truncateMiddle } from "./Utils";
-import { useComponentValue, useRows } from "@latticexyz/react";
+import { useRows } from "@latticexyz/react";
 import React from 'react';
 
 export const TopUser = () => {
     const {
-        components: { Achievements },
-        systemCalls: { setAchievement },
-        network: { playerEntity, network, storeCache },
+        network: { storeCache },
     } = useMUD();
 
     const achievements = useRows(storeCache, { table: "Achievements" });
 
     function getMedal(number: number): string {
         if (number === 1) {
-          return "🥇";
+            return "🥇";
         } else if (number === 2) {
-          return "🥈";
+            return "🥈";
         } else if (number === 3) {
-          return "🥉";
+            return "🥉";
         } else {
-          return number.toString();
+            return number.toString();
         }
-      }
+    }
 
-      function formatValue(value: string): string {
-        const formattedValue = value.replace(/^0x0+/, '0x');
-  return formattedValue;
-      }
+    function formatValue(value: string): string {
+        return value.replace(/^0x0+/, '0x');
+    }
 
     return (
-        <List style={{ width: "100%", height: "100%", backgroundColor: "#ffffff", marginTop: "21px", borderRadius: "15px" }}>
-            {achievements.sort((a, b) => {
-                return b.value.total - a.value.total
-            })
+        <List style={{ 
+            width: "100%", 
+            height: "100%", 
+            backgroundColor: "#ffffff", 
+            marginTop: "21px", 
+            borderRadius: "15px" 
+        }}>
+            {achievements.sort((a, b) => { return b.value.total - a.value.total })
                 .map((data, index) => (
                     <ListItem style={{
                         height: "91px",
@@ -46,16 +47,28 @@ export const TopUser = () => {
                         marginTop: "16px"
                     }}>
                         <div style={{
-                            width: "56px", height: "56px", borderRadius: "35.5px", backgroundColor: "#ffffff", textAlign: "center",
-                            lineHeight: "56px", fontSize: "20px"
+                            width: "56px", 
+                            height: "56px", 
+                            borderRadius: "35.5px", 
+                            backgroundColor: "#ffffff", 
+                            textAlign: "center",
+                            lineHeight: "56px", 
+                            fontSize: "20px"
                         }}>
-                            {getMedal(index + 1)}
+                            { getMedal(index + 1) }
                         </div>
-                        <div style={{ flex: "1", marginLeft: "35px", textOverflow: "ellipsis" }}>
-                            {truncateMiddle(formatValue(data.key.key), 20)}
+                        <div style={{ 
+                            flex: "1",
+                             marginLeft: "35px", 
+                             textOverflow: "ellipsis" 
+                        }}>
+                            { truncateMiddle(formatValue(data.key.key), 20) }
                         </div>
-                        <div style={{ flexBasis: "10%", textAlign: "right" }}>
-                            {data.value.total}
+                        <div style={{ 
+                            flexBasis: "10%", 
+                            textAlign: "right" 
+                        }}>
+                            { data.value.total }
                         </div>
                     </ListItem>
                 ))}
