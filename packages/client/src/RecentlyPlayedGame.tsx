52e435a404cefe7d70a6d5b9b2e3e37bf2ecc6f7
Hakyung Kim <hkhk4862@gmail.com>
2023-05-23 15:07:55 +0900

    RecentlyPlayedGame

--- packages/client/src/RecentlyPlayedGame.tsx
+++ packages/client/src/RecentlyPlayedGame.tsx
@@ -3,34 +3,20 @@ import { useMUD } from "./MUDContext";
 import { useComponentValue, useRows } from "@latticexyz/react";
 import React from 'react';
 
-function truncateMiddle(text: string, maxLength: number): string {
-    if (text.length <= maxLength) {
-        return text;
-    }
-
-    const ellipsis = '...';
-    const truncatedLength = maxLength - ellipsis.length;
-    const startLength = Math.ceil(truncatedLength / 2);
-    const endLength = Math.floor(truncatedLength / 2);
-
-    const truncatedText =
-        text.substr(0, startLength) + ellipsis + text.substr(-endLength);
-
-    return truncatedText;
-}
-
 export const RecentlyPlayedGame = () => {
-    const {
-        components: { Achievements },
-        systemCalls: { setAchievement },
-        network: { playerEntity, network, storeCache },
-    } = useMUD();
-
-    const achievements = useRows(storeCache, { table: "Achievements" });
-
     return (
         <List style={{ width: "100%", height: "100%", backgroundColor: "#ffffff", marginTop: "21px", borderRadius: "15px" }}>
-
+            <ListItem style={{
+                height: "91px",
+                width: "90%",
+                backgroundColor: "rgba(230, 233, 241, 0.35)",
+                borderRadius: "15px",
+                display: "flex",
+                marginLeft: "24px",
+                marginTop: "16px"
+            }}>
+                {/* TODO */}
+            </ListItem>
         </List>
     );
 };
